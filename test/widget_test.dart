50be66b4d00b054e348df5051700e6952b4d8487
zahranzill <2206027@itg.ac.id>
2025-05-19 14:41:38 +0700

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,24 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:newshivez/main.dart';
+import 'package:newshivez/views/introduction_screen.dart';
+import 'package:newshivez/views/splash_screen.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('Splash screen shows and navigates to introduction screen',
+      (WidgetTester tester) async {
+    // Jalankan aplikasi
+    await tester.pumpWidget(const NewsHiveApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.byType(SplashScreen), findsOneWidget);
+    expect(find.text('NewsHive'), findsOneWidget);
+    expect(find.byIcon(Icons.newspaper_rounded), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    await tester.pump(const Duration(seconds: 2));
+    await tester.pumpAndSettle(); // biar selesai semua animasi/navigasi
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.byType(IntroductionScreen), findsOneWidget);
+    expect(find.text('Stay Informed, Anytime'), findsOneWidget);
+    expect(find.text('Get Started'), findsOneWidget);
   });
 }
